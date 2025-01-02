a5d4d508383fcc6958e76edcfc90836bdd0a675d
joao victor benedet machado <jvbm2177@gmail.com>
2025-01-02 12:05:17 -0300

    exercicios de print formatado

--- src/CursoUdemy/Main.java
+++ src/CursoUdemy/Main.java
@@ -2,6 +2,21 @@ package CursoUdemy;
 
 public class Main {
     public static void main(String[] args) {
+        int y = 32;
+        double x = 3.14159;
+
         System.out.println("Hello Word!");
+        System.out.print("Ola mundo");
+        System.out.println(y);
+        System.out.printf("%.2f\n", x);
+
+
+
+        Print("Ola mundo");
+
+    }
+
+    public static void Print(Object a) {
+        System.out.println(a);
     }
 }
