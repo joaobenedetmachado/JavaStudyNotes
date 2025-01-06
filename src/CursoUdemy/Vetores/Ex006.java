8b8633bb787185965ea516141857c0de870c0031
joao victor benedet machado <jvbm2177@gmail.com>
2025-01-06 14:16:37 -0300

    vetores

---
+++ src/CursoUdemy/Vetores/Ex006.java
@@ -0,0 +1,28 @@
+package CursoUdemy.Vetores;
+
+import java.util.Scanner;
+
+public class Ex006 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine();
+        Estudantes[] quartos = new Estudantes[10]; /* 10 quartos por padrao no caso*/
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("RESERVA " + (i+ 1));
+            String nome = sc.nextLine();
+            String email = sc.nextLine();
+            int quarto = sc.nextInt();
+            sc.nextLine();
+            quartos[quarto] = new Estudantes(nome, email, quarto);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (quartos[i] != null) {
+                System.out.println(quartos[i]);
+            }
+        }
+    }
+}
