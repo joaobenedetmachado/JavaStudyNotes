24f4afdb15789e496a0528154dfd7f444809ccb1
joao victor benedet machado <jvbm2177@gmail.com>
2025-01-07 12:50:43 -0300

    for each

--- src/CursoUdemy/ForEach/Ex001.java
+++ src/CursoUdemy/ForEach/Ex001.java
@@ -1,4 +1,11 @@
 package CursoUdemy.ForEach;
 
 public class Ex001 {
+    public static void main(String[] args) {
+        String[] vetor = new String[] {"Maria", "João", "Lucas"};
+
+        for (String x : vetor) {
+            System.out.println("a" + x);
+        }
+    }
 }
