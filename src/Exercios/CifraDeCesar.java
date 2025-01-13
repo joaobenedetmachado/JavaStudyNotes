fd70ffbff98240d1ea292fd25fc29be74b73744f
joao victor benedet machado <jvbm2177@gmail.com>
2025-01-13 01:01:36 -0300

    exercicios - cifra de cesar

---
+++ src/Exercios/CifraDeCesar.java
@@ -0,0 +1,34 @@
+package Exercios;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class CifraDeCesar {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String palavra = sc.nextLine();
+        List<Character> WordToArray = new ArrayList<>();
+        List<String> alfabeto = new ArrayList<>();
+
+        for (char letra = 'a'; letra <= 'z'; letra++) {
+            alfabeto.add(String.valueOf(letra));
+        }
+
+        for (int i = 0; i < palavra.length(); i++) {
+            WordToArray.add(palavra.charAt(i));
+        }
+        System.out.println();
+
+        for (int x = 0; x < WordToArray.size(); x++) {
+            String letraParaBuscar = String.valueOf(WordToArray.get(x));
+            int posicao = alfabeto.indexOf(letraParaBuscar);
+            int novaPosicao = (posicao + 2) % 26; // modulo para que nunca ultrapasse de 26 caracteres e assim voltando
+            System.out.print(alfabeto.get(novaPosicao));
+        }
+    }
+}
