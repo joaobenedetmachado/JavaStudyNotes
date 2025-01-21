492158cf1eaf49b3d01c5f25e69161396de9796d
joao victor benedet machado <jvbm2177@gmail.com>
2025-01-21 11:34:36 -0300

    tratamento de erros com try - catch

--- src/CursoUdemy/TryCatch/Ex001.java
+++ src/CursoUdemy/TryCatch/Ex001.java
@@ -1,10 +1,18 @@
 package CursoUdemy.TryCatch;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ex001 {
     public static void main(String[] args) {
 
+        method2();
+
+        System.out.println("Fim do PROGRama");
+    }
+
+    public static void method2() {
+        System.out.println("-*-*-*-*- INICIO DO METODO 2 -*-*-*-*-");
         Scanner sc = new Scanner(System.in);
 
         try {
@@ -13,9 +21,10 @@ public class Ex001 {
             System.out.println(vect[position]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("erro! posicao nao existe!");
+        } catch (InputMismatchException e) {
+            System.out.println("Posicao nao encontrada, erro: " + e );
         }
-
-        System.out.println("Fim do PROGRama");
         sc.close();
+        System.out.println("-*-*-*-*- FINAL DO METODO 2 -*-*-*-*-");
     }
 }
