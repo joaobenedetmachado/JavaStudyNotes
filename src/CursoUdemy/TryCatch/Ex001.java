6acadb184d0a7767c0a5648680191b0d076bc3ab
joao victor benedet machado <jvbm2177@gmail.com>
2025-01-21 11:38:46 -0300

    e.printStackTrace()

--- src/CursoUdemy/TryCatch/Ex001.java
+++ src/CursoUdemy/TryCatch/Ex001.java
@@ -6,11 +6,18 @@ import java.util.Scanner;
 public class Ex001 {
     public static void main(String[] args) {
 
-        method2();
+        method1();
 
         System.out.println("Fim do PROGRama");
     }
 
+    public static void method1() {
+        System.out.println("-*-*-*-*- INICIO DO METODO 1 -*-*-*-*-");
+        Scanner sc = new Scanner(System.in);
+        method2();
+        sc.close();
+        System.out.println("-*-*-*-*- FINAL DO METODO 1 -*-*-*-*-");
+    }
     public static void method2() {
         System.out.println("-*-*-*-*- INICIO DO METODO 2 -*-*-*-*-");
         Scanner sc = new Scanner(System.in);
@@ -21,6 +28,7 @@ public class Ex001 {
             System.out.println(vect[position]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("erro! posicao nao existe!");
+            //e.printStackTrace(); // o print stack mostra onde que esta o erro e o caminho que o codigo levou ate chegar no erro
         } catch (InputMismatchException e) {
             System.out.println("Posicao nao encontrada, erro: " + e );
         }
