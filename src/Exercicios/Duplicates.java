ebe754f76af999db4f9aa7809dfa391a0cb78b07
joao victor benedet machado <jvbm2177@gmail.com>
2025-02-12 16:33:05 -0300

    Remove duplicates in a array

---
+++ src/Exercicios/Duplicates.java
@@ -0,0 +1,25 @@
+package Exercicios;
+
+import java.util.ArrayList;
+
+public class Duplicates {
+    public static void main(String[] arg) {
+        ArrayList<Integer> mainArray = new ArrayList<>();
+        mainArray.add(1);
+        mainArray.add(2);
+        mainArray.add(3);
+        mainArray.add(3);
+        mainArray.add(7);
+        mainArray.add(7);
+
+        ArrayList<Integer> NewArray = new ArrayList<>();
+
+        for (int i = 0; i < mainArray.size(); i++) {
+            if (!NewArray.contains(mainArray.get(i))) {
+                NewArray.add(mainArray.get(i));
+            }
+        }
+
+        System.out.println(NewArray);
+    }
+}
