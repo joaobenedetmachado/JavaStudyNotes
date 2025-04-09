08537af8be79e798939724ee0abad22d62a313d2
João Victor Benedet Machado <jvbm2177@gmail.com>
2025-04-09 09:00:06 -0300

    criptografia - terminar dps

---
+++ src/HomeStudies/Criptog/Ex001.java
@@ -0,0 +1,11 @@
+package HomeStudies.Criptog;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.nio.charset.StandardCharsets;
+public class Ex001 {
+    public static void main(String[] args) {
+        Usuario user1 = new Usuario("Joao", "senha123");
+
+
+    }
+}
